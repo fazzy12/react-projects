9e6ee64c0ff9a5dcfc17d1f44281c2247155b015
fazzy12 <kaluifeanyi00@gmail.com>
2022-11-05 13:52:54 +0100

    finish building site

--- react-info-site/src/main.js
+++ react-info-site/src/main.js
@@ -3,14 +3,17 @@ import React from 'react';
 export default function Main() {
     return (
         <main>
-            <h1 className='main--tittle'>Fun Facts About React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
+            <div className='main--container'>
+
+                <h1 className='main--tittle'>Fun Facts About React</h1>
+                <ul className='main--facts'>
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100K stars on GitHub</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </div>
         </main>
     );
 }
--- react-info-site/src/navbar.js
+++ react-info-site/src/navbar.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Navbar() {
     return (
         <nav>
-            <img src="../public/logo192.png" alt='logo' className='nav--logo'/>
+            <img src="./images/logo192.png" alt='logo' className='nav--logo'/>
             <h3 className='nav--logo_text'>ReactFacts</h3>
             <h4 className='nav--tittle'>react course-project 1</h4>
         </nav>
