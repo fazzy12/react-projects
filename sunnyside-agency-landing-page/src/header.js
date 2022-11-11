33408851b80b7ecad833aa78f3d437f785e92293
fazzy12 <kaluifeanyi00@gmail.com>
2022-11-11 23:06:12 +0100

    update file structure

---
+++ sunnyside-agency-landing-page/src/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './navbar';
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <Navbar/>
+            
+        </header>
+    );
+}
+
---
+++ sunnyside-agency-landing-page/src/navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Navbar() {
+    return(
+        <img src='../images/logo.svg' alt="logo icon"/>
+    );
+}
