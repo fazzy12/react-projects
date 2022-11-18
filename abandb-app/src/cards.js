df262473ec4f9baff4e0cc1070e35d90ca41eb2d
fazzy12 <kaluifeanyi00@gmail.com>
2022-11-18 06:39:05 +0100

    airbandb update

--- abandb-app/src/cards.js
+++ abandb-app/src/cards.js
@@ -1,35 +1,42 @@
 import Img1 from './images/image12.png';
+import Img2 from './images/wedding-photography 1.png'
+import Img3 from './images/mountain-bike 1.png';
 import Star from './images/Star.png';
 
 const rate = '5.0 (6)-USA';
 const nuggets = 'Life lessons with Katie Zaferes';
-const price = 'from $135/person';
 
 export default function Card() {
     return (
-        <section>
-            <img src={Img1} alt="sport woman" />
+        <div className='card'>
+            <img src={Img1} alt="sport woman" className='card--image'/>
+            <div className='card--stats'>
+                <img src={Star} alt='start svg' className='card--star' /> 
+                <span>5.0</span>
+                <span classname='gray' >(6)-</span>
+                <span className='gray'>USA</span>
+            </div>
+                <p>{nuggets}</p>
+                <p> <span className='bold'>from $135</span> / person</p>
+
+
+
+
+            <img src={Img2} alt="woman on wedding dress" />
             <div>
                 <img src={Star} alt='start svg' /> <p>{rate}</p>
                 <p>{nuggets}</p>
-                <p>{price}</p>
+               
 
             </div>
-            <img src={Img1} alt="sport woman" />
+            <img src={Img3} alt="bicycle" />
             <div>
                 <img src={Star} alt='start svg' /> <p>{rate}</p>
                 <p>{nuggets}</p>
-                <p>{price}</p>
+               
 
             </div>
-            <img src={Img1} alt="sport woman" />
-            <div>
-                <img src={Star} alt='start svg' /> <p>{rate}</p>
-                <p>{nuggets}</p>
-                <p>{price}</p>
-
-            </div>
-        </section>
+        </div>
 
     );
 
--- sunnyside-agency-landing-page/src/main.js
+++ sunnyside-agency-landing-page/src/main.js
@@ -8,8 +8,8 @@ export default function Main() {
         <section className='main--section-1'>
             <div className='main--section-1_left'>
                 <div className='main--section-text'>
-                <h1>Transform your brand</h1>
-                <p>We are a full-service creative agency specializing in helping brands grow fast. 
+                <h1 className='main--section-header'>Transform your brand</h1>
+                <p className='main--section-para'>We are a full-service creative agency specializing in helping brands grow fast. 
                 Engage your clients through compelling visuals that do most of the marketing for you.</p>
                 <a href="http://www.jjjd.com">Learn more</a>
                 </div>
@@ -22,12 +22,12 @@ export default function Main() {
 
         <section className='main--section-2'>
             <div className='main--section-2_left'>
-                <img src="../images/desktop/image-transform.jpg" alt='the egg'/>
+                <img src="../images/desktop/image-stand-out.jpg" alt='the egg'/>
             </div>
             <div className='main--section-2_right'>
                 <div className='main--section-text'>
-                <h1>Stand out to the right audience</h1>
-                <p>Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.</p>
+                <h1 className='main--section-header'>Stand out to the right audience</h1>
+                <p className='main--section-para'>Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.</p>
                 <a href="http://www.jjjd.com">Learn more</a>
                 </div>
             </div>
